bd06c981d5f317659ebe07f9177d4d52b41ee910
Belikov <lvb@spsbk.ru>
2018-01-20 01:22:43 +0300

    * project upd
    ~ bar-descriptor
    ~ license notes
    - /localization

--- src/applicationui.cpp
+++ src/applicationui.cpp
@@ -1,20 +1,22 @@
 /*
  * Copyright (c) 2017-2018 Arfeo.net.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "applicationui.hpp"
+#include "telegram.h"
+#include "config.h"
 
 using namespace bb::cascades;
 
@@ -25,10 +27,6 @@ ApplicationUI::ApplicationUI() :
     // to ensure the document gets destroyed properly at shut down
     QmlDocument *qml = QmlDocument::create("asset:///main.qml").parent(this);
     qml->setContextProperty("_telega", this);
-
-    // Enable application settings
-    QCoreApplication::setOrganizationName("Arfeo.net");
-    QCoreApplication::setApplicationName("Telega");
 
     // Create root object for the UI
     AbstractPane *root = qml->createRootObject<AbstractPane>();
--- src/config.h
+++ src/config.h
@@ -1,2 +1,18 @@
+/*
+ * Copyright (c) 2017-2018 Arfeo.net.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define TELEGA_API_ID 120542
 #define TELEGA_API_HASH "255a0340af74254c1b3a2d0a62de0373"
